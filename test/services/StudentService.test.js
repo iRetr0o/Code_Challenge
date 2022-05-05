ed4f3b668e83b830b1ef3919741fe33e4bab8129
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-05-05 16:49:57 -0500

    Prueba 2 StudentService: Filtro por certificacion

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -8,4 +8,14 @@ describe("Prueba al StudentService", () => {
         ]);
         expect(students.length).toEqual(2);
     });
+    test("Lista con emails de partners que tienen certificación", () => {
+        const students = StudentService.getStudents([
+            {name: "Partner1", haveCertification: true, email: "1@gmail.com"},
+            {name: "Partnert2", haveCertification: false, email: "2@gmail.com"},
+            {name: "Partner3", haveCertification: true, email: "3@gmail.com"},
+            {name: "Partnert4", haveCertification: false, email: "4@gmail.com"}
+        ]);
+        const certificationList = StudentService.getStudentsEmailByCertification(students);
+        expect(certificationList.length).toBe(2);
+    });
 });
