13d7aef6fc2d48accdff602401aef6ad3ddfe9b5
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-05-06 13:21:11 -0500

    Prueba 1 StudentController: Regresando un partner

---
+++ test/controllers/StudentController.test.js
@@ -0,0 +1,19 @@
+const StudentController = require("../../lib/controllers/StudentController");
+
+describe("Prueba al StudentController", () => {
+    test("Regresando un partner de la database", () => {
+        const partner = StudentController.getPartners();
+        expect(partner[43]).toMatchObject({
+            "id": "6264d5d86119fd599c5a67e7",
+            "name": "Tillman",
+            "email": "Kerri@visualpartnership.xyz",
+            "credits": 600,
+            "enrollments": [
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 5,
+            "haveCertification": false
+        });
+    });
+});
